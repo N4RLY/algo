f50cc69ef7ddfbaeb6cece63751e68266a703289
N4RLY <71723309+N4RLY@users.noreply.github.com>
2022-07-26 13:35:28 +0300

    Add files via upload

---
+++ IsPrime.cpp
@@ -0,0 +1,21 @@
+// #define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+
+bool IsPrime(ll x){
+    if(x == 1) return false;
+    for(ll i = 2; i*i <= x; ++i){
+        if(x % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    cout << IsPrime(5) << '\n';
+    cout << IsPrime(18) << '\n';
+    cout << IsPrime(19);
+    return 0;
+}
