a2b7a497eff3c7cd1809a61b3fe765d816170755
N4RLY <71723309+N4RLY@users.noreply.github.com>
2022-11-16 13:17:43 +0300

    a

--- Z.cpp
+++ Z.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
 
-vector<long long>  zfunc(string s){
-    long long n=s.size();
-    long long l,r;
+vector<ll>  zfunc(string s){
+    ll n=s.size();
+    ll l,r;
     l=0; r=1;
-    vector<long long> z(n,0);
+    vector<ll> z(n,0);
 
-    for (long long i=1; i<n; ++i){
+    for (ll i=1; i<n; ++i){
         if (r>i) z[i]=min(z[i-l],r-i); //установка нижней границы
         while (z[i]+i<n && s[z[i]+i]==s[z[i]]) z[i]++; //наивный алгоритм
         if (i+z[i]>r) {l=i; r=i+z[i];} //пересчет l и r
