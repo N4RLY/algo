9f5401a918134f378e04976d8f5855d830970ee4
Narly <aleminee2@gmail.com>
2024-02-12 10:54:37 +0300

    qq

---
+++ qq.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,popcnt,abm,mmx,tune=native")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize "-O3"
+using namespace std;
+typedef long long int ll;
+#define forn(i,n) for (int i = 0; i < int(n); i++)
+
+void solve(){
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int t=1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+
